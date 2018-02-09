b13aff735a3bd2cad0cf5594a517370303e48ca4
conor <sullivanc@hosemaster.com>
2018-02-09 08:16:48 -0500

    first

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
+import { RestProvider } from './../../providers/rest/rest';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	@ViewChild(Slides) slides: Slides;
+	data:any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public restProvider: RestProvider) {
+  	this.getGifs(0);
+  }
 
+  slideChanged() {
+    if(this.slides.isEnd()){
+    	this.getGifs(this.data.length);
+    }
+  }
+
+  getGifs(offset){
+  	this.restProvider.getData(offset)
+  		.subscribe(res => {
+  			if(offset > 0){
+  				res.forEach(obj =>{
+  					this.data.push(obj);
+  				});
+  				
+  				console.log('getting more', this.data)
+  			}else{
+  				this.data = res;
+  				console.log('less');
+  			}
+
+  		});
   }
 
 }
---
+++ src/providers/rest/rest.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the RestProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class RestProvider {
+	url:string;
+	offset:number;
+	limit:number = 10;
+	query:string = 'sad';
+	apiKey:string = 'dc6zaTOxFJmzC';
+
+  constructor(public http: HttpClient) {
+    this.url = 'http://api.giphy.com/v1/gifs/search?q='+this.query+'&limit='+this.limit+'&offset=';
+
+  }
+
+  getData(offset){
+  	return this.http.get(this.url+offset+'&api_key='+this.apiKey)
+  		.map(res => (res['data']));
+  }
+
+}
